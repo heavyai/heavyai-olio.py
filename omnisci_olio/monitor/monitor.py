2c9e21461aac17614e3fbdb4d4ebf0f2c4a2533a
Mike Hinchey <mike.hinchey@omnisci.com>
2019-12-18 22:16:36 -0800

    monitor: container col, use free
    add container id column
    use free instead of top, which uses + for very large mem
    logging

--- omnisci_olio/monitor/monitor.py
+++ omnisci_olio/monitor/monitor.py
@@ -19,10 +19,10 @@ import ibis
 import omnisci_olio.pymapd
 import omnisci_olio.ibis
 
-
 import logging
 logging.basicConfig()
-log = logging.getLogger('default')
+log = logging.getLogger('omnisci_monitor')
+log.setLevel('DEBUG')
 
 
 def disk_used(path):
@@ -33,21 +33,22 @@ def disk_used(path):
     return (int(used / 1024), 1 - st.f_bavail / st.f_blocks)
 
 
-def sys_metrics():
+def sys_metrics(con):
     df = pd.DataFrame({
         'timestamp_': [pd.datetime.now()],
-        # 'hostname': [socket.gethostname()],
+        'container': con.con._client.get_status(con.con._session)[0].host_name,
         'hostname': os.environ.get('MONITOR_HOSTNAME', None)
     })
+
+    freeout = subprocess.check_output(['free'], text=True).split('\n')
+    df['cpu_mem'] = int(freeout[1].split()[2])
+    df['cpu_swap'] = int(freeout[2].split()[2])
     
-    topout = subprocess.check_output(['top', '-b', '-n1'], text=True).split('\n')[:5]
+    # topout = subprocess.check_output(['top', '-b', '-n1'], text=True).split('\n')[:5]
     # df['cpu_loadavg'] = float(topout[0].split('load average: ')[1].split(',')[0])
     # cpu_us = topout[2].split('%Cpu(s):')[1].split(',')[0].strip()
-
-    df['cpu_mem'] = int(topout[3].split(',')[2].strip().split(' ')[0].strip())
-
-    df['cpu_swap'] = int(topout[4].split(',')[2].strip().split(' ')[0].strip())
-    df
+    # df['cpu_mem'] = int(topout[3].split(',')[2].strip().split(' ')[0].strip())
+    # df['cpu_swap'] = int(topout[4].split(',')[2].strip().split(' ')[0].strip())
 
     with open('/proc/loadavg') as f:
         df['cpu_load'] = float(f.read().split(' ')[0])
@@ -99,18 +100,26 @@ def gpu_summary(gpu):
 
 #
 # All metrics
-# 
+#
 
 
 sum_schema = ibis.schema(
-    names=['timestamp_', 'hostname', 'cpu_mem', 'cpu_swap', 'cpu_load', 'disk_used_kb', 'disk_used_pct',
-           'gpu_pct_avg', 'gpu_mem_pct_avg', 'gpu_mem_used_mib', 'gpu_power_draw_w', 'gpu_proc_temp_c',
-          'db_cpu_mem_alloc_kb', 'db_cpu_mem_used_kb', 'db_gpu_mem_alloc_kb', 'db_gpu_mem_used_kb'],
-    types=['timestamp', 'string', 'float32', 'int64', 'int64', 'float32', 'int64', 'float32', 'float32', 'float32', 'int32', 'float32', 'int16', 'int32', 'int32', 'int32', 'int32'])
+    names=['timestamp_', 'hostname',
+        'cpu_mem', 'cpu_swap', 'cpu_load',
+        'disk_used_kb', 'disk_used_pct',
+        'gpu_pct_avg', 'gpu_mem_pct_avg', 'gpu_mem_used_mib',
+        'gpu_power_draw_w', 'gpu_proc_temp_c',
+        'db_cpu_mem_alloc_kb', 'db_cpu_mem_used_kb',
+        'db_gpu_mem_alloc_kb', 'db_gpu_mem_used_kb',
+        'container',
+        ],
+    types=['timestamp', 'string', 'float32', 'int64', 'int64',
+        'float32', 'int64', 'float32', 'float32', 'float32', 'int32',
+        'float32', 'int16', 'int32', 'int32', 'int32', 'int32', 'string'])
 
 
 def all_metrics(con):
-    df = sys_metrics()
+    df = sys_metrics(con)
     gpu = gpu_metrics()
     s = gpu_summary(gpu)
     for k, v in s.items():
@@ -122,10 +131,16 @@ def all_metrics(con):
         df['db_' + row.device_type + '_mem_alloc_kb'] = int(row.page_size * row.pages_allocated / 1024)
         df['db_' + row.device_type + '_mem_used_kb'] = int(row.page_size * row.used_pages / 1024)
     
-    # print('all_metrics df', df)
+    # log.debug('all_metrics df %s', df)
     
-    df = sum_schema.apply_to(df)
+    sum_schema.apply_to(df)
+    df = df[sum_schema.names]
+    # log.debug(f'all_metrics apply_to {df.to_csv()}')
     return {'summary': df, 'gpu': gpu}
+
+
+# ## Load to OmniSci
+# 
 
 
 summary_table = 'omnisci_system_metrics_summary'
@@ -138,30 +153,32 @@ def create_tables(tgt):
     if not tgt.exists_table(summary_table):
         tgt.create_table(summary_table, schema=sum_schema, max_rows=10**9*200)
 
+    # TODO evolve old schema:
+    # alter table omnisci_system_metrics_summary add container text
+
+
 #
 # Run Forever
 #
-
-
 def monitor_import(sleep_seconds=1, batch=100, tgt_file=None):
     df = None
     errors = 0
     while True:
         with ibis.omniscidb.connect(os.environ['OMNISCI_DB_URL']) as src:
             while True:
-                # print(db_memory(src, detail=0))
+                # log.debug(db_memory(src, detail=0))
                 try:
                     df = pd.concat([df, all_metrics(src)['summary']], sort=False)
                     if len(df) >= batch:
+                        if tgt_file:
+                            with open(tgt_file, 'a') as f:
+                                print(df.to_csv(f, header=False))
                         if 'OMNISCI_DB_URL_TGT' in os.environ:
                             with ibis.omniscidb.connect(os.environ['OMNISCI_DB_URL_TGT']) as tgt:
                                 create_tables(tgt)
                                 tgt.load_data(summary_table, df)
                                 t = tgt.table(summary_table)
-                                print(f'Loaded {len(df)}, count={t.count().execute()}', file=sys.stderr)
-                        if tgt_file:
-                            with open(tgt_file, 'a') as f:
-                                print(df.to_csv(f, header=False))
+                                log.info(f'Loaded {len(df)}, count={t.count().execute()}')
                         df = None
                         errors = 0
                     else:
@@ -170,7 +187,7 @@ def monitor_import(sleep_seconds=1, batch=100, tgt_file=None):
                     errors += 1
                     if errors >= 10:
                         if df:
-                            print('Failed to insert metrics: ' + df.to_csv(), file=sys.stderr)
+                            log.error(f'Failed to insert metrics: {df.to_csv()}')
                         raise
-                    print(f'Continuing after load error: {e} {df}', file=sys.stderr)
+                    log.error(f'Continuing after load error: {e} {df}')
                     break # break from the inner while, to re-connect with src
